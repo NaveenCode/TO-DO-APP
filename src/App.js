a342191423de4894237c1ee6fa77e8243b1e7aae
Naveen Kumar <73029naveenkumar73537@gmail.com>
2023-10-23 19:37:48 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,85 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import TodoInput from './components/TodoInput';
 import { useState } from 'react';
 import TodoList from './components/TodoList';
 
 function App(props) {
-  const [listTodo,setListTodo]=useState([]);
-  const addList=(inputText)=>{
-    if(inputText!=='')
-        setListTodo([...listTodo,inputText])
+  const [listTodo, setListTodo] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [inputText, setInputText] = useState('');
+  const [isEditItem, setIsEditItem] = useState(null);
+
+  const addList = () => {
+    if (!inputText) {
+      alert('Please Enter Todo');
+      return;
+    }
+
+    if (toggle) {
+      setListTodo([...listTodo, inputText]);
+    } else {
+      setListTodo(
+        listTodo.map((elem, index) => {
+          if (index === isEditItem) {
+            return inputText;
+          }
+          return elem;
+        })
+      );
+    }
+
+    setInputText('');
+    setToggle(true);
+    setIsEditItem(null);
   }
-  const onDeleteList=(key)=>{
-    const newList=[...listTodo]
-    newList.splice(key,1)
-    setListTodo(newList)
+
+  const editTodo = (index) => {
+    const findItem = listTodo[index];
+    setToggle(false);
+    setInputText(findItem);
+    setIsEditItem(index);
   }
-  const deleteAllItems=()=>{
-       setListTodo([])
+
+  const onDeleteList = (key) => {
+    const newList = listTodo.filter((item, index) => index !== key);
+    setListTodo(newList);
   }
+
+  const deleteAllItems = () => {
+    setListTodo([]);
+  }
+
   return (
     <div className="main-container">
-      <div className="center-conatiner">
-        <TodoInput addList={addList}/>
+      <div className="center-container">
+        <div className="input-container">
+          <input
+            type="text"
+            className="input-box-todo"
+            placeholder="Enter your todo"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+          />
+          {toggle ? (
+            <button className="add-btn" onClick={addList}>
+              +
+            </button>
+          ) : (
+            <button className='add-btn' onClick={addList}>edit</button>
+          )}
+        </div>
         <h1 className='app-heading'>TODO</h1>
-        <hr className='text-white'/>
-        {listTodo.map((listItem,index)=>{
-          return(<TodoList index={index} key={index} item={listItem} deletItems={onDeleteList} />)
-        })}
+        <hr className='text-white' />
+        {listTodo.map((listItem, index) => (
+          <TodoList
+            index={index}
+            key={index}
+            item={listItem}
+            deletItems={onDeleteList}
+            editTodo={editTodo}
+          />
+        ))}
         <div className='d-flex justify-content-center'>
-          <button className='delete-all rounded' onClick={()=>{deleteAllItems(props.index)}}>Delete All</button>
+          <button className='delete-all rounded' onClick={() => { deleteAllItems(props.index) }}>Delete All</button>
         </div>
       </div>
     </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { RiDeleteBin6Line } from 'react-icons/ri';
+import { AiTwotoneEdit } from 'react-icons/ai';
 
 
 function TodoList(props) {
   return (
     <li className="list-item">
-         {props.item}
-        <span className="icons">
-            
-            <RiDeleteBin6Line className='icon-delete' onClick={()=>{props.deletItems(props.index)}}/>
-        </span>
+      {props.item}
+      <span className="icons">
+        <AiTwotoneEdit className='icon-edit' onClick={() => props.editTodo(props.index)} />
+        <RiDeleteBin6Line className='icon-delete' onClick={() => { props.deletItems(props.index) }} />
+      </span>
     </li>
   )
 }
